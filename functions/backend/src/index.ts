56f6e4d4454553ea729f6b6ca778b1d4288ec789
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2025-01-31 15:45:23 -0500

    backend project setup

--- functions/backend/src/index.ts
+++ functions/backend/src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello world mah man");
+import "module-alias/register";
+import cors from "cors";
+import express from "express";
+// import * as functions from "firebase-functions";
+import router from "@/routers/index";
+import logging from "@/utils/logging";
+import { errorHandler } from "@/middlewares/errorHandler";
+import { loggingHandler } from "@/middlewares/loggingHandler";
+import { CLIENT_ORIGIN, SERVER_PORT } from "@/configs/config";
+
+const app = express();
+app.use(cors({
+    origin: CLIENT_ORIGIN,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+app.use(express.json());
+
+
+const appRouter = router();
+
+app.use(loggingHandler);
+
+app.options("*", cors({
+    origin: CLIENT_ORIGIN,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+
+app.use("/api", appRouter);
+
+
+app.use(errorHandler);
+
+
+app.listen(SERVER_PORT, () => {
+    logging.log(`Server running on port ${SERVER_PORT}`);
+});
+
+// exports.app = functions.https.onRequest(app);
