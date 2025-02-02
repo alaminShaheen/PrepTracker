de30ddb017e4ff6859632e1ec2254355c06e2259
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2025-02-02 14:51:34 -0500

    added authentication to backend

---
+++ functions/backend/src/controllers/AuthController.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+
+import { AuthService } from "@/services/AuthService";
+import { RegisterUserDto } from "@/models/dtos/RegisterUserDto";
+import { LoginRequestDto } from "@/models/dtos/LoginRequestDto";
+import { handleFormValidationErrors } from "@/utils/throwValidationErrors";
+
+async function loginHandler(request: Request<{}, {}, LoginRequestDto>, response: Response, next: NextFunction) {
+    try {
+        handleFormValidationErrors(request);
+        const loginResponse = await AuthService.login(request.body, response);
+        response.status(200).json(loginResponse);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function registerHandler(request: Request<{}, {}, RegisterUserDto>, response: Response, next: NextFunction) {
+    try {
+        handleFormValidationErrors(request);
+        await AuthService.register(request.body);
+        response.sendStatus(200);
+        return;
+    } catch (error: any) {
+        next(error);
+    }
+}
+
+async function registerOAuthHandler(request: Request, response: Response, next: NextFunction) {
+    try {
+        const user = await AuthService.registerOAuthUser(request.userInfo);
+        response.status(200).json(user);
+        return;
+    } catch (error: any) {
+        next(error);
+    }
+}
+
+async function registerPasswordReset(request: Request<{}, {}, RegisterUserDto>, response: Response, next: NextFunction) {
+    try {
+        handleFormValidationErrors(request);
+        await AuthService.resetPassword(request.body);
+        response.sendStatus(200);
+        return;
+    } catch (error: any) {
+        next(error);
+    }
+}
+
+export const AuthController = {
+    loginHandler,
+    registerHandler,
+    registerPasswordReset,
+    registerOAuthHandler,
+};
