45500249a9e9e0dcf9c6d70199742efd51f1fa22
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2025-02-14 19:21:15 -0500

    removed a function

--- functions/backend/src/utils/dateUtils.ts
+++ functions/backend/src/utils/dateUtils.ts
@@ -3,8 +3,3 @@ import { isAfter, isBefore, isEqual, isSameDay } from "date-fns";
 export function isDateInBetweenRange(startDate: Date, endDate: Date, referenceDate: Date) {
     return (isSameDay(referenceDate, startDate) || isAfter(referenceDate, startDate)) && (isSameDay(referenceDate, endDate) || isBefore(referenceDate, endDate));
 }
-
-export function doRangesOverlap(start1: Date, end1: Date, start2: Date, end2: Date) {
-    return isBefore(start1, end2) && isAfter(end1, start2);
-
-}
